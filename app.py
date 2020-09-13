b9db1556321566499d6bb1a1c9fa67881f756a85
Acher-Larh <achlarhmich@gmail.com>
2020-09-13 13:51:42 +0200

    Created the 'register' table, and coded the script to add some info to the table.

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-#SPH = Silver Per Hour
+from database_create import create_register, create_material, create_connection, show_database
+from datetime import date
+
 def calculateIncome():
+    today = date.today()
+
     materialPrices = {
     "Red Meat" : 9900,
     "Caphras Stone" : 2450000,
@@ -7,7 +11,9 @@ def calculateIncome():
     "Hard Crystal Shard" : 1400000,
     "Ancient Spirit Dust" : 490000, 
     "Fairy's Powder" : 10000,
-    "Black Gem Fragment" : 251000 
+    "Black Gem Fragment" : 251000,
+    "Courser Stuff" : 2490000,
+    "Energy Pots" : 4500000
     }
 
     redMeat = int(input("Red Meat Gathered -> ")) or 0
@@ -17,6 +23,8 @@ def calculateIncome():
     spiritDust = int(input("Ancient Spirit Dust Gathered -> ")) or 0
     fairyPowder = int(input("Fairy's Powder Gathered -> ")) or 0
     blackGemFrags = int(input("Black Gem Fragment Gathered -> ")) or 0
+    courserStuff = int(input("Courser Stuff Gathered -> ")) or 0
+    energyPots = int(input("Energy Pots Used -> ")) or 0
 
     redMeatSilver = materialPrices["Red Meat"] * redMeat
     caphrasStoneSilver = materialPrices["Caphras Stone"] * caphrasStones
@@ -25,22 +33,31 @@ def calculateIncome():
     spiritDustSilver = materialPrices["Ancient Spirit Dust"] * spiritDust
     fairyPowderSilver = materialPrices["Fairy's Powder"] * fairyPowder
     blackGemFragSilver = materialPrices["Black Gem Fragment"] * blackGemFrags
+    courserStuffSilver = materialPrices["Courser Stuff"] * courserStuff
+    energyPotsSilver = materialPrices["Energy Pots"] * energyPots
 
-    print(redMeatSilver)
-    print(caphrasStoneSilver)
-    print(sharpShardSilver)
-    print(hardShardSilver)
-    print(spiritDustSilver)
-    print(fairyPowderSilver)
-    print(blackGemFragSilver)
+    # print(redMeatSilver)
+    # print(caphrasStoneSilver)
+    # print(sharpShardSilver)
+    # print(hardShardSilver)
+    # print(spiritDustSilver)
+    # print(fairyPowderSilver)
+    # print(blackGemFragSilver)
 
-    totalTaxedSilver = (redMeatSilver + caphrasStoneSilver + sharpShardSilver + hardShardSilver + spiritDustSilver + blackGemFragSilver)*0.845 + fairyPowderSilver
+    totalTaxedSilver = (redMeatSilver + caphrasStoneSilver + sharpShardSilver + hardShardSilver + spiritDustSilver + blackGemFragSilver + courserStuffSilver) * 0.845 + fairyPowderSilver - energyPotsSilver
 
-
+ 
     totalTime = input("Input the time it took you to gather those materials -> ")
+    silverPerHour = getSilverPerHour(totalTaxedSilver, totalTime)
     print("##################################")
     print("total(taxed): ", totalTaxedSilver)
-    print("Your income per hour is: ", getSilverPerHour(totalTaxedSilver, totalTime))
+    print("Your income per hour is: ", silverPerHour)
+
+    conn = create_connection("database.db")
+    register_1 = (str(totalTaxedSilver)+" Silver(Session)", str(silverPerHour)+" Silver(Per Hour)", today.strftime("%d/%m/%Y"), totalTime+" minutes")
+    create_register(conn, register_1)
+
+    show_database(conn)
 
 def getSilverPerHour(totalTaxedSilver, totalTime):
     return 60*int(totalTaxedSilver)/int(totalTime)
--- database_create.py
+++ database_create.py
@@ -16,25 +16,49 @@ def create_table(conn, create_table_sql):
     try:
         c = conn.cursor()
         c.execute(create_table_sql)
-        print(c.fetchall())
     except Error as e:
         print(e)
 
-def show_database():
-    pass
+    
+def show_database(conn):
+
+    c = conn.cursor()
+
+    c.execute("""SELECT * FROM register""")
+    print(c.fetchall())
+
+def create_material(conn, material):
+    sql =""" INSERT INTO materials(name, price) VALUES(?,?) """
+    cur = conn.cursor()
+    cur.execute(sql, material)
+    conn.commit()
+    
+    return cur.lastrowid
+
+def create_register(conn, register):
+    sql =""" INSERT INTO register(name, value, date, time) VALUES(?,?,?,?) """
+
+    cur = conn.cursor()
+    cur.execute(sql, register)
+    conn.commit()
+    
+    return cur.lastrowid
+
 def main():
     database = "database.db"
 
     sql_create_materials_table = """ CREATE TABLE IF NOT EXISTS materials (
-    id integer PRIMARY KEY,
-    name text NOT NULL,
-    price integer NOT NULL
+        id integer PRIMARY KEY,
+        name text NOT NULL,
+        price integer NOT NULL
     );"""
 
     sql_create_register_table = """ CREATE TABLE IF NOT EXISTS register (
         id integer PRIMARY KEY, 
         name text NOT NULL, 
-        value integer NOT NULL
+        value text NOT NULL,
+        date text,
+        time text
     );"""
 
     conn = create_connection(database)
@@ -43,6 +67,8 @@ def main():
         create_table(conn, sql_create_materials_table)
       
         create_table(conn, sql_create_register_table)
+
+        show_database(conn)
     else: 
         print("Error! Cannot create the database connection.")
 
